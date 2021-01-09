19c2cdf6122183ef13b4b0635674ccee4fe96205
askbk <askbk@protonmail.com>
2021-01-09 19:20:14 +0100

    Add shape parameter and more tests.

--- PegSolitaire/Board.py
+++ PegSolitaire/Board.py
@@ -1,6 +1,8 @@
 class Board:
-    def __init__(self, hole_count=1, size=4):
+    def __init__(self, hole_count=1, size=4, shape="triangle"):
         if hole_count < 1:
             raise ValueError("hole_count must be at least 1")
         if size < 4:
             raise ValueError("size must be at least 4")
+        if shape != "triangle" and shape != "diamond":
+            raise ValueError("shape must be either 'triangle' or 'diamond'")
--- PegSolitaire/Board_test.py
+++ PegSolitaire/Board_test.py
@@ -2,6 +2,12 @@ import pytest
 from Board import Board
 
 
+def test_board_constructor():
+    Board()
+    Board(hole_count=2, size=4, shape="triangle")
+    Board(hole_count=3, size=5, shape="diamond")
+
+
 def test_board_constructor_exceptions():
     with pytest.raises(ValueError):
         Board(hole_count=0)
@@ -10,3 +16,8 @@ def test_board_constructor_exceptions():
     for size in sizes:
         with pytest.raises(ValueError):
             Board(size=size)
+
+    shapes = ["", "square"]
+    for shape in shapes:
+        with pytest.raises(ValueError):
+            Board(shape=shape)
