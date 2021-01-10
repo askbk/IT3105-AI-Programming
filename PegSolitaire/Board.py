3666952b658f782d8809561aed103f58f0c04de9
askbk <askbk@protonmail.com>
2021-01-10 14:50:09 +0100

    Add get_movable_pieces method.

--- PegSolitaire/Board.py
+++ PegSolitaire/Board.py
@@ -1,4 +1,7 @@
-from typing import List, Tuple
+from typing import List, Tuple, TYPE_CHECKING
+from itertools import product, accumulate
+
+Position = Tuple[int]
 
 
 class Board:
@@ -10,14 +13,25 @@ class Board:
         if shape != "triangle" and shape != "diamond":
             raise ValueError("shape must be either 'triangle' or 'diamond'")
 
-        for position in hole_positions:
-            if not Board._position_is_valid(shape, size, position):
-                raise ValueError
+        if any(
+            map(
+                lambda position: not Board._position_is_valid(shape, size, position),
+                hole_positions,
+            )
+        ):
+            raise ValueError("one or more invalid hole positions")
 
-        self.size = size
+        if len(set(hole_positions)) != len(hole_positions):
+            raise ValueError("one or more duplicate hole positions")
+
+        self._size = size
+        self._shape = shape
+        self._hole_positions = hole_positions
 
     @staticmethod
-    def _position_is_valid(board_shape, board_size, position):
+    def _position_is_valid(
+        board_shape: str, board_size: int, position: Position
+    ) -> bool:
         """
         Determines whether a position is valid for a given board shape and size.
         """
@@ -31,14 +45,75 @@ class Board:
 
         return True
 
-    # def get_movable_pieces(self) -> List[Tuple[int]]:
-    #     """
-    #     Get currently movable pieces as a list of tuples of their positions.
-    #     """
-    #     result = []
-    #     for row in range(self.size):
-    #         for col in range(self.size):
-    #             if can_move_to_hole((row, col)):
-    #                 result += (row, col)
+    def _get_all_valid_positions(self) -> List[Position]:
+        """
+        Returns a list of all valid positions on the game board.
+        """
+        return list(
+            filter(
+                lambda position: Board._position_is_valid(
+                    self._shape, self._size, position
+                ),
+                product(range(self._size), range(self._size)),
+            )
+        )
 
-    #     return result
+    def _are_on_same_row(self, position_a: Position, position_b: Position) -> bool:
+        return position_a[0] == position_b[0]
+
+    def _are_on_same_column(self, position_a: Position, position_b: Position) -> bool:
+        return position_a[1] == position_b[1]
+
+    def _are_on_same_diagonal(self, position_a: Position, position_b: Position) -> bool:
+        return position_a[0] + position_a[1] == position_b[0] + position_b[1]
+
+    def _straight_distance(self, position_a, position_b):
+        if self._are_on_same_column(position_a, position_b) or self._are_on_same_row(
+            position_a, position_b
+        ):
+            return abs(position_a[0] - position_b[0]) + abs(
+                position_a[1] - position_b[1]
+            )
+
+        if not self._are_on_same_diagonal(position_a, position_b):
+            raise ValueError("there is no straight line between the positions")
+
+        return abs(position_a[0] - position_b[0])
+
+    def _can_move_to_hole(
+        self, piece_position: Position, hole_position: Position
+    ) -> bool:
+        """
+        Determines whether a piece can move into a given hole.
+        """
+        if not (
+            self._are_on_same_column(piece_position, hole_position)
+            or self._are_on_same_row(piece_position, hole_position)
+            or self._are_on_same_diagonal(piece_position, hole_position)
+        ):
+            return False
+
+        result = self._straight_distance(piece_position, hole_position) == 2
+
+        if result:
+            print(piece_position, "can move to", hole_position)
+
+        return result
+
+    def get_movable_pieces(self) -> List[Position]:
+        """
+        Get currently movable pieces as a list of tuples of their positions.
+        """
+        return list(
+            filter(
+                lambda position: any(
+                    map(
+                        lambda hole_position: self._can_move_to_hole(
+                            position, hole_position
+                        ),
+                        self._hole_positions,
+                    )
+                ),
+                self._get_all_valid_positions(),
+            )
+        )
--- PegSolitaire/Board_test.py
+++ PegSolitaire/Board_test.py
@@ -4,7 +4,7 @@ from Board import Board
 
 def test_board_constructor():
     Board()
-    Board(hole_count=2, size=5, shape="triangle")
+    Board(hole_count=2, size=5, shape="triangle", hole_positions=[(1, 2), (1, 3)])
 
 
 def test_board_constructor_exceptions():
@@ -24,10 +24,13 @@ def test_board_constructor_exceptions():
     with pytest.raises(ValueError):
         Board(shape="triangle", size=5, hole_positions=[(4, 2)])
 
+    with pytest.raises(ValueError):
+        Board(shape="triangle", size=5, hole_positions=[(1, 1), (1, 1)])
 
-# def test_board_movable_pieces():
-#     assert sorted(
-#         Board(
-#             hole_count=1, size=4, shape="diamond", hole_positions=[(1, 1)]
-#         ).get_movable_pieces()
-#     ) == sorted([(1, 3), (3, 1)])
+
+def test_get_movable_pieces():
+    assert sorted(
+        Board(
+            hole_count=1, size=4, shape="diamond", hole_positions=[(1, 1)]
+        ).get_movable_pieces()
+    ) == sorted([(1, 3), (3, 1)])
