dbdc088da323c2d8a1ee4ae14c55e95b58de9cbb
askbk <askbk@protonmail.com>
2021-01-18 12:10:50 +0100

    Add parameters to constructor.

--- PegSolitaire/Critic.py
+++ PegSolitaire/Critic.py
@@ -3,4 +3,12 @@ class Critic:
     The Critic part of the Actor-Critic model
     """
 
-    pass
+    def __init__(
+        self,
+        critic_function="table",
+        critic_nn_dimensions=None,
+        critic_learning_rate=0.9,
+        critic_eligibility_decay_rate=0.9,
+        critic_discount_factor=0.9,
+    ):
+        pass
--- PegSolitaire/Critic_test.py
+++ PegSolitaire/Critic_test.py
@@ -3,3 +3,10 @@ from Critic import Critic
 
 def test_constructor():
     Critic()
+    Critic(
+        critic_function="table",
+        critic_nn_dimensions=None,
+        critic_learning_rate=0.9,
+        critic_eligibility_decay_rate=0.9,
+        critic_discount_factor=0.9,
+    )
