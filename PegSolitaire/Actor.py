730a27bc0ad5d8ea25819ddb47dfbecb8d1e8c62
askbk <askbk@protonmail.com>
2021-01-18 12:09:54 +0100

    Add actor parameters to constructor.

--- PegSolitaire/Actor.py
+++ PegSolitaire/Actor.py
@@ -3,4 +3,12 @@ class Actor:
     The Actor part of the Actor-Critic model.
     """
 
-    pass
+    def __init__(
+        self,
+        actor_discount_factor=0.9,
+        actor_eligibility_decay_rate=0.9,
+        actor_learning_rate=0.01,
+        initial_epsilon=0.05,
+        epsilon_decay_rate=0.9,
+    ):
+        pass
--- PegSolitaire/Actor_test.py
+++ PegSolitaire/Actor_test.py
@@ -3,3 +3,10 @@ from Actor import Actor
 
 def test_constructor():
     Actor()
+    Actor(
+        actor_discount_factor=0.9,
+        actor_eligibility_decay_rate=0.9,
+        actor_learning_rate=0.01,
+        initial_epsilon=0.05,
+        epsilon_decay_rate=0.9,
+    )
