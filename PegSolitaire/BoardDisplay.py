afc277f62dbfb086ce33556185106af9d9b86a95
askbk <askbk@protonmail.com>
2021-01-18 09:31:31 +0100

    Refactor.

--- PegSolitaire/BoardDisplay.py
+++ PegSolitaire/BoardDisplay.py
@@ -37,29 +37,42 @@ class BoardDisplay:
             )
         ).astype("float64")
 
-        rotated_and_reflected = BoardDisplay._rotate(position_matrix)
+        rotated = BoardDisplay._rotate(position_matrix)
 
-        return dict(zip(board_positions, rotated_and_reflected))
+        return dict(zip(board_positions, rotated))
 
     @staticmethod
-    def display_board_with_move(board: Board, move: Tuple[Position]):
+    def _draw_holes(graph, node_positions, holes):
         """
-        Displays a board state while emphasising the previous move.
+        Draws the holes on the board.
         """
-        edges = board.get_edge_list()
-        graph = Graph(edges)
-        holes = board.get_all_holes()
-        pieces = board.get_all_pieces()
-        from_position, to_position = move
-        node_positions = BoardDisplay._calculate_node_positions(holes + pieces)
-
-        draw_networkx_edges(graph, pos=node_positions)
         draw_networkx_nodes(
             graph, pos=node_positions, nodelist=holes, node_size=75, node_color="black"
         )
+
+    @staticmethod
+    def _draw_edges(graph, node_positions):
+        """
+        Draws the edges of the board.
+        """
+        draw_networkx_edges(graph, pos=node_positions)
+
+    @staticmethod
+    def _draw_pieces(graph, node_positions, pieces):
+        """
+        Draws the game pieces on the board.
+        """
         draw_networkx_nodes(
             graph, pos=node_positions, nodelist=pieces, node_color="blue"
         )
+
+    @staticmethod
+    def _draw_move(graph, node_positions, move: Tuple[Position]):
+        """
+        Draws the board transition caused by a move.
+        """
+        from_position, to_position = move
+
         draw_networkx_nodes(
             graph,
             pos=node_positions,
@@ -75,12 +88,30 @@ class BoardDisplay:
             node_size=500,
         )
 
+    @staticmethod
+    def display_board_with_move(board: Board, move: Tuple[Position]):
+        """
+        Displays a board state while emphasising the previous move.
+        """
+        edges = board.get_edge_list()
+        graph = Graph(edges)
+        holes = board.get_all_holes()
+        pieces = board.get_all_pieces()
+        node_positions = BoardDisplay._calculate_node_positions(holes + pieces)
+
+        BoardDisplay._draw_edges(graph, node_positions)
+        BoardDisplay._draw_holes(graph, node_positions, holes)
+        BoardDisplay._draw_pieces(graph, node_positions, pieces)
+        BoardDisplay._draw_move(graph, node_positions, move)
+
         plt.draw()
         plt.axis("off")
         plt.savefig(fname="board.png")
 
 
 BoardDisplay.display_board_with_move(
-    Board(size=4, hole_positions=[Position((1, 2)), Position((1, 3))]),
+    Board(
+        size=5, shape="triangle", hole_positions=[Position((1, 2)), Position((1, 3))]
+    ),
     (Position((1, 3)), Position((1, 1))),
 )
