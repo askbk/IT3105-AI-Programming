c30520ed1f0f2a945c6fca5cc0990897b6a92338
askbk <askbk@protonmail.com>
2021-02-13 12:00:09 +0100

    Cleanup

--- PegSolitaire/Play.py
+++ PegSolitaire/Play.py
@@ -1,37 +1,16 @@
 from Agent.ACMAgent import ACMAgent, agent_diamond_4, agent_nn_diamond_4
-from Game.Board import Board, diamond_4, triangle_5
-from Game.Position import Position
+from Game.Board import (
+    diamond_3,
+    diamond_4,
+    diamond_5,
+    triangle_3,
+    triangle_4,
+    triangle_5,
+)
 from PerformanceDisplay import graph_performance
 from Player import Player
 
-
-diamond_4_params_table = {
-    "actor_discount_factor": 0.9,
-    "actor_eligibility_decay_rate": 0.9,
-    "actor_learning_rate": 0.001,
-    "critic_discount_factor": 0.5,
-    "critic_eligibility_decay_rate": 0.5,
-    "critic_learning_rate": 0.001,
-    "epsilon_decay_rate": 0.9,
-    "initial_epsilon": 1,
-}
-
-diamond_4_params_table_2 = {
-    "actor_discount_factor": 0.7,
-    "actor_eligibility_decay_rate": 0.8,
-    "actor_learning_rate": 0.01,
-    "critic_discount_factor": 0.4,
-    "critic_eligibility_decay_rate": 0.4,
-    "critic_learning_rate": 0.001,
-    "epsilon_decay_rate": 0.96,
-    "initial_epsilon": 1,
-}
-
-
 if __name__ == "__main__":
-    agent = ACMAgent(**diamond_4_params_table_2)
-    player = Player(agent)
-
-    graph_performance(player.play_multiple_episodes(diamond_4, 100))
+    graph_performance(Player(agent_diamond_4).play_multiple_episodes(diamond_4, 100))
     # graph_performance(Player(agent_nn_diamond_4).play_multiple_episodes(diamond_4, 100))
     # graph_performance(Player(ACMAgent(**a)).play_multiple_episodes(triangle_5, 400))
