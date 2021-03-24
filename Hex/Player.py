4da2f8e1beab0fbee71eb6f18f12400c35ca6e33
Ask Berstad Kolltveit <askbk@protonmail.com>
2021-03-24 18:27:57 +0100

    Remove empty docstrings

--- Hex/Player.py
+++ Hex/Player.py
@@ -3,24 +3,12 @@ from Game import GameBase
 
 
 class Player:
-    """
-    docstring
-    """
 
     def get_action(self, current_state: GameBase):
-        """
-        docstring
-        """
         return random.choice(current_state.get_possible_actions())
 
     def next_state(self, next_state: GameBase):
-        """
-        docstring
-        """
         return Player()
 
     def end_of_episode_update(self):
-        """
-        docstring
-        """
         return Player()
