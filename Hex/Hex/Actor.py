748a8b0dc6cad5f264ac41043fa012583a2f604a
Ask Berstad Kolltveit <askbk@protonmail.com>
2021-03-30 14:07:55 +0200

    Check input shapes

--- Hex/Hex/Actor.py
+++ Hex/Hex/Actor.py
@@ -10,6 +10,8 @@ import tensorflow.keras as keras
 
 class Actor:
     def __init__(self, input_size: int, output_size: int):
+        self._input_size = input_size
+        self._output_size = output_size
         inputs = keras.layers.Input(shape=(input_size,))
         self._nn = keras.Model(
             inputs=inputs,
@@ -26,12 +28,22 @@ class Actor:
         self._nn.compile(optimizer="adam", loss="mean_squared_error")
 
     def rollout(self, state_vector: StateVector) -> np.array:
-        return self._nn(np.atleast_2d(np.array(state_vector))).numpy().flatten()
+        x = np.atleast_2d(np.array(state_vector))
+        if x.shape != (1, self._input_size):
+            raise ValueError(
+                f"Input must be a vector of length {self._input_size}, length was {x.shape[1]}"
+            )
+        return self._nn(x).numpy().flatten()
 
     def train(self, replay_buffer: ReplayBuffer) -> Actor:
         root_states, probability_distributions = zip(*replay_buffer)
-        self._nn.train_on_batch(
-            x=np.atleast_2d(np.array(root_states)),
-            y=np.atleast_2d(np.array(probability_distributions)),
-        )
+        x = np.atleast_2d(np.array(root_states))
+        y = np.atleast_2d(np.array(probability_distributions))
+        if x.shape[1] != self._input_size:
+            raise ValueError(f"Input vectors must have length {self._input_size}")
+
+        if y.shape[1] != self._output_size:
+            raise ValueError(f"Output vectors must have length {self._output_size}")
+
+        self._nn.train_on_batch(x=x, y=y)
         return self
