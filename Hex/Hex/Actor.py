fe9d7cf7dadb021b5a37888a673f1933e4b9113e
Ask Berstad Kolltveit <askbk@protonmail.com>
2021-03-30 10:41:11 +0200

    Implement actor training method

--- Hex/Hex/Actor.py
+++ Hex/Hex/Actor.py
@@ -23,8 +23,15 @@ class Actor:
             ),
         )
 
+        self._nn.compile(optimizer="adam", loss="mean_squared_error")
+
     def rollout(self, state_vector: StateVector) -> np.array:
         return self._nn(np.atleast_2d(np.array(state_vector))).numpy().flatten()
 
     def train(self, replay_buffer: ReplayBuffer) -> Actor:
+        root_states, probability_distributions = zip(*replay_buffer)
+        self._nn.train_on_batch(
+            x=np.atleast_2d(np.array(root_states)),
+            y=np.atleast_2d(np.array(probability_distributions)),
+        )
         return self
