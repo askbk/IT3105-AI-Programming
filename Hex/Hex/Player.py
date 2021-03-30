51c53b3dd0e6352722cfdc8c5a39ee9638a688d3
Ask Berstad Kolltveit <askbk@protonmail.com>
2021-03-30 12:06:19 +0200

    Remove keyword arg, add option to display board

--- Hex/Hex/Player.py
+++ Hex/Hex/Player.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from Hex.Game import GameBase
+from Hex.Game import GameBase, BoardDisplay
 from Hex.Utils import while_loop
 from Hex.AgentBase import AgentBase
 from Hex.Agent import Agent
@@ -27,9 +27,7 @@ class Player:
             state_sequence, agent = state
             current_state = state_sequence[-1]
             next_state = current_state.perform_action(agent.get_action(current_state))
-            return [*state_sequence, next_state], agent.next_state(
-                next_state, initial=False
-            )
+            return [*state_sequence, next_state], agent.next_state(next_state)
 
         initial = (
             [self._game],
@@ -39,6 +37,11 @@ class Player:
         state_sequence, agent = while_loop(condition, initial, step)
 
         return state_sequence, agent.end_of_episode_update()
+
+    def play_single_episode(self, display_board: bool = True, time_interval: int = 1):
+        boards, _ = self._play_single_episode()
+        if display_board:
+            BoardDisplay.display_board_sequence(boards, pause=time_interval)
 
     def play_episodes(self, episode_count: int = 1):
         self._play_single_episode()
