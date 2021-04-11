b265ce191e077d7e72a30e15f5183686bb6735e1
askbk <askbk@protonmail.com>
2021-04-11 18:20:07 +0200

    Add time limit support for MCTS

--- Hex/Hex/MCTS/MCTS.py
+++ Hex/Hex/MCTS/MCTS.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import math
 import random
 import operator
+import time
 from functools import reduce
 from typing import Optional, Dict, Callable, Sequence
 from Hex.Game import GameBase
@@ -19,6 +20,7 @@ class MCTS:
         initial_state: Optional[GameBase] = None,
         search_games: int = 100,
         exploration_coefficient: float = 3,
+        time_limit: float = 1,
         _distribution=[],
         _tree: Tree = None,
     ):
@@ -26,6 +28,7 @@ class MCTS:
         self._search_games = search_games
         self._tree = Tree(initial_state) if _tree is None else _tree
         self._exploration_coefficient = exploration_coefficient
+        self._time_limit = time_limit
 
     @staticmethod
     def from_config(mcts_config: Dict, initial_state: GameBase):
@@ -33,6 +36,7 @@ class MCTS:
             initial_state=initial_state,
             search_games=mcts_config.get("search_games", 100),
             exploration_coefficient=mcts_config.get("exploration_coefficient", 3),
+            time_limit=mcts_config.get("time_limit", 1),
         )
 
     def get_root_distribution(self) -> list:
@@ -65,7 +69,10 @@ class MCTS:
 
     @staticmethod
     def _tree_search(
-        rollout_policy: RolloutPolicy, tree_policy: TreePolicy
+        rollout_policy: RolloutPolicy,
+        tree_policy: TreePolicy,
+        start_time: float,
+        time_limit: float,
     ) -> Callable[[Tree, int], Tree]:
         def perform_rollout(state: GameBase) -> int:
             """
@@ -82,10 +89,14 @@ class MCTS:
         def perform_search(tree: Tree, game_number: int) -> Tree:
             """
             Return new tree with updated statistics.
+            Stops if time limit is reached.
             """
             # 1. follow tree policy until unvisited node reached
             # 2. perform rollout from node
             # 3. update tree with reward from rollout
+            if time.time() - start_time > time_limit:
+                return tree
+
             if not tree.is_visited() or tree.is_end_state():
                 reward = perform_rollout(tree.get_state())
                 return tree.increment_visit_count(reward=reward)
@@ -107,7 +118,10 @@ class MCTS:
 
         new_tree = reduce(
             MCTS._tree_search(
-                rollout_policy, MCTS._tree_policy(self._exploration_coefficient)
+                rollout_policy,
+                MCTS._tree_policy(self._exploration_coefficient),
+                start_time=time.time(),
+                time_limit=self._time_limit,
             ),
             range(self._search_games + 1),
             self._tree,
